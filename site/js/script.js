3005d231b78bc674ee822d0e64aa67a8a304b2f7
Jonathas Cavalcante <jonathas.cavalcante@outlook.com.br>
2024-06-19 22:10:35 -0300

    Update ports

--- site/js/script.js
+++ site/js/script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   var instances = M.Modal.init(elems);
 });
 
-const url = "http://localhost:4000/";
+const url = "http://0.0.0.0:4000/";
 
 let getCard = () => {
   // Limpa a seleção de cartas anteriores e remove a classe pulsar do botão
